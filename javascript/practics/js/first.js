96a03599e25818b9e08508186f5d5b8915048c58
sanjaysheel <sheel1997sanjay@gmail.com>
2020-08-27 14:37:16 +0530

    all done

---
+++ javascript/practics/js/first.js
@@ -0,0 +1,34 @@
+// console.time('your code works')
+// console.log('your code work')
+// console.log(14)
+// console.log(14 + 4)
+// console.log(true)
+// console.log(true)
+// console.warn('this is first warning')
+// console.timeEnd('your code works')
+
+// console.log('tut3')
+// var name = 'sanjay';
+// var channel;
+// channel = 'sanjay2'
+// console.log
+// var $city;
+// $city = 'hello'
+// console.log($city)
+
+
+var name = 'sanjay'
+var channel;
+var marks = 3454;
+marks = 0;
+console.log(name, channel, marks)
+
+var city = 'delhi';
+console.log(city);
+const ownerName = 'harry ram';
+// ownerName = 'hello';
+console.log(ownerName);
+
+{
+
+}
