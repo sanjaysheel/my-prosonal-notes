605c816be99bc4d389c9c8b6885a0f3c927fb11a
sanjaysheel <sheel1997sanjay@gmail.com>
2020-09-05 18:37:59 +0530

    new added in javasctipt

--- javascript/practics/js/first.js
+++ javascript/practics/js/first.js
@@ -17,18 +17,55 @@
 // console.log($city)
 
 
-var name = 'sanjay'
-var channel;
-var marks = 3454;
-marks = 0;
-console.log(name, channel, marks)
+// var name = 'sanjay'
+// var channel;
+// var marks = 3454;
+// marks = 0;
+// console.log(name, channel, marks)
 
-var city = 'delhi';
-console.log(city);
-const ownerName = 'harry ram';
-// ownerName = 'hello';
-console.log(ownerName);
+// var city = 'delhi';
+// console.log(city);
+// const ownerName = 'harry ram';
+// // ownerName = 'hello';
+// console.log(ownerName);
 
-{
 
-}
+// console.time('your code Took')
+// console.log('your code took')
+// console.log(4 + 34)
+// console.log(true)
+// console.log([34, 2, 12, 21, 43, 34])
+// console.log({ 'harry': 2, 'yashbant': 21, 'raj': 34 })
+// console.timeEnd('your code Took')
+// console.assert(255 > 189, 'age>189 is not possible')
+// console.error('this is an error')
+
+
+// console.log('first time declaring variabel in javascript')
+// var name = 'john';
+// console.log(name)
+// var name = 'john';
+// console.log(name)
+// name_ = 'sohan'
+// console.log(name_)
+// console.clear()
+
+// const $name = 'john';
+// // ___name__ = 'abs' 
+// // 
+// // 
+// //      It will give an error  if you try to change 
+// //      const variable
+// // 
+// // 
+// console.log($name)
+// name_ = 'sohan'
+// console.log(name_)
+
+
+
+
+
+
+
+console.assert
